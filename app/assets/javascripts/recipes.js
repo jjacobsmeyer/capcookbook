85f99b47b3a429451e1b12a31082b6b13e9f96c5
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-10-22 16:16:19 -0500

    User Story 15 completed, Toggle Comments

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $('#comment-box').hide();
+
+  $("#open-comment-link").click(function(e) {
+    e.preventDefault();
+    $('#comment-box').slideToggle();
+  });
+});
